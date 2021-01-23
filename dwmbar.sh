99b1a44e858014e4e5dad530210b09e6ab7f3736
Udit Karode <udit.karode@gmail.com>
2021-01-23 22:46:26 +0530

    dwmbar: use variable accent color

--- dwmbar.sh
+++ dwmbar.sh
@@ -10,6 +10,7 @@ BATTERY="$(acpi | sed "s/,//g" | awk '{if ($3 == "Discharging"){print $4; exit} 
 MEM="$(free | awk 'FNR == 2 {print $3/($3+$4)*100}')"
 WLAN="$(nmcli dev status | grep 'wlp59s0' | grep -v 'p2p-dev' | awk '{ print $3 }')"
 DATE="$(date '+%a, %I:%M:%S %p')"
+COLOR="$(cat ~/.dwm_status_color)"
 
 if [ "$WLAN" != "disconnected" ]; then
 	WLAN="$(nmcli dev status | grep 'wlp59s0' | grep -v 'p2p-dev' | awk '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }')"
@@ -17,5 +18,5 @@ fi
 
 sleep 1
 
-xsetroot -name "^c#7cafc2^ ^c#a9a9a9^VOL ^c#ffffff^$VOLUME ^c#7cafc2^[] ^c#a9a9a9^BAT ^c#ffffff^$BATTERY ^c#7cafc2^[] ^c#a9a9a9^MEM ^c#ffffff^$MEM% ^c#7CAFC2^[] ^c#a9a9a9^WLAN ^c#ffffff^$WLAN^c#7cafc2^^b#1089ff^^c#ffffff^ > $DATE < "
+xsetroot -name "^c#7cafc2^ ^c#a9a9a9^VOL ^c#ffffff^$VOLUME ^c$COLOR^[] ^c#a9a9a9^BAT ^c#ffffff^$BATTERY ^c$COLOR^[] ^c#a9a9a9^MEM ^c#ffffff^$MEM% ^c$COLOR^[] ^c#a9a9a9^WLAN ^c#ffffff^$WLAN^c#7cafc2^^b$COLOR^^c#ffffff^ > $DATE < "
 done
