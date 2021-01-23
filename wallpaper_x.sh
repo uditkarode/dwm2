df85a9478d28be8a97f02ba7fb1e6eb57a6b9e4e
Udit Karode <udit.karode@gmail.com>
2021-01-23 22:46:49 +0530

    wallpaper_x: script to change wallpaper and color scheme

---
+++ wallpaper_x.sh
@@ -0,0 +1,15 @@
+#!/bin/dash
+
+if [ -d "$1" ]; then
+	cd $1
+	target="$(ls $1 | shuf -n 1)"
+	echo "$(md5sum ~/.config/wallpaper.png | awk '{ print $1 }')"
+	echo "$(md5sum $target | awk '{ print $1 }')"
+	while [ "$(md5sum ~/.config/wallpaper.png | awk '{ print $1 }')" = "$(md5sum $target | awk '{ print $1 }')" ]; do
+		target="$(ls $1 | shuf -n 1)"
+	done
+	wal -i "$target"
+	xdotool key "Super+F5"
+	COLOR="$(xrdb -query | grep -i '*color10' | head -n 1 | sed 's/.*://' | sed 's/^\s//')"
+	echo "$COLOR" > ~/.dwm_status_color
+fi
