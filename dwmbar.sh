0fd23a645c807845e82cbc8d54a8e3bcda60ab1b
Udit Karode <udit.karode@gmail.com>
2021-01-30 20:36:17 +0530

    dwmbar: account data fetch time for updation sleep

--- dwmbar.sh
+++ dwmbar.sh
@@ -1,5 +1,8 @@
-#!/bin/dash
+#!/bin/zsh
+
 while true; do
+
+FETCH_START="$(($(date +%s%N)/1000000))"
 
 if [ -z "$(ps -au $USER | grep -i dwm)" ]; then
     break
@@ -10,6 +13,8 @@ BATTERY="$(acpi | sed "s/,//g" | awk '{if ($3 == "Discharging"){print $4; exit} 
 MEM="$(free | awk 'FNR == 2 {print $3/($3+$4)*100}')"
 WLAN="$(nmcli dev status | grep 'wlp59s0' | grep -v 'p2p-dev' | awk '{ print $3 }')"
 DATE="$(date '+%a, %I:%M:%S %p')"
+POWER="$(powercheck)"
+
 COLOR="$(cat ~/.dwm_status_color)"
 SINK="$(pactl info | grep -i 'sink' | sed 's/Default Sink: //')"
 SOURCE="$(pactl info | grep -i 'source' | sed 's/Default Source: //')"
@@ -31,11 +36,20 @@ fi
 
 AUDIO_DEVICE="${AUDIO_DEVICE}]"
 
+if [ "$(pamixer --get-mute)" = "true" ]; then
+	AUDIO_DEVICE="${AUDIO_DEVICE}[MUTE]"
+fi
+
 if [ "$WLAN" != "disconnected" ]; then
 	WLAN="$(nmcli dev status | grep 'wlp59s0' | grep -v 'p2p-dev' | awk '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }')"
 fi
 
-sleep 1
+FETCH_END="$(($(date +%s%N)/1000000))"
+DIFF="$(( $FETCH_END - $FETCH_START ))"
 
-xsetroot -name "^c#7cafc2^ ^c#a9a9a9^VOL ^c#ffffff^$VOLUME $AUDIO_DEVICE ^c$COLOR^[] ^c#a9a9a9^BAT ^c#ffffff^$BATTERY ^c$COLOR^[] ^c#a9a9a9^MEM ^c#ffffff^$MEM% ^c$COLOR^[] ^c#a9a9a9^WLAN ^c#ffffff^$WLAN^c#7cafc2^^b$COLOR^^c#ffffff^ > $DATE < "
+if [ "$DIFF" -lt "1000" ]; then
+	sleep "$(( $DIFF.0 / 1000 ))"
+fi
+
+xsetroot -name "^c#7cafc2^ ^c#a9a9a9^VOL ^c#ffffff^$VOLUME $AUDIO_DEVICE ^c$COLOR^[] ^c#a9a9a9^BAT ^c#ffffff^$BATTERY ^c$COLOR^[] ^c#a9a9a9^PWR ^c#ffffff^$POWER^c$COLOR^[] ^c#a9a9a9^MEM ^c#ffffff^$MEM% ^c$COLOR^[] ^c#a9a9a9^WLAN ^c#ffffff^$WLAN^c#7cafc2^^b$COLOR^^c#ffffff^ > $DATE < "
 done
