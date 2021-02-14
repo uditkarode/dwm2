e1c36fae6345d7cf163acfc96d4f736e15d3e038
Udit Karode <udit.karode@gmail.com>
2021-02-14 18:05:53 +0530

    config: set default browser to firefox

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-dpi", "90", "-theme", "slate", "-lines", "12","-padding", "18", "-width", "60", "-location", "0", "-show", "run"};
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *browsercmd[] = {"chromium"};
+static const char *browsercmd[] = {"firefox"};
 
 
 #include <X11/XF86keysym.h>
@@ -97,7 +97,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("prime-run java -jar /home/udit/Minecraft/TLauncher-2.72.jar") },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("prime-run java -jar /home/udit/Minecraft/TLauncher-2.75.jar") },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
