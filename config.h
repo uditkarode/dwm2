c03de1b21887776021c14812a2c88d1db6702f02
Rob Pilling <robpilling@gmail.com>
2021-02-21 23:24:04 +0530

    dwm: alwaysontop patch

--- config.h
+++ config.h
@@ -123,6 +123,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_o,      togglealwaysontop, {0} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
